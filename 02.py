80fca58ee5e77e19b6bcec9a26f34c5cfb37b486
fox34 <github@noecho.de>
2020-12-02 18:10:21 +0100

    Tag 2 optimiert

--- 02.py
+++ 02.py
@@ -16,13 +16,9 @@ def password_is_valid_old_rules(dataset):
     # - password: string
     # - ruleLetter: string
     # - lowerLimit, upperLimit: int
-    foundOccurences = 0
-    for letterInPassword in password:
-        if letterInPassword == ruleLetter:
-            foundOccurences += 1
     
     # Anzahl Buchstaben zwischen beiden Limits
-    return lowerLimit <= foundOccurences <= upperLimit
+    return lowerLimit <= password.count(ruleLetter) <= upperLimit
 
 
 # Teil 1: Anzahl korrekter Passwörter
@@ -45,32 +41,8 @@ def password_is_valid_new_rules(dataset):
     positions, ruleLetter = rule.split(" ", 1)
     firstPos, secondPos = [int(num) for num in positions.split("-", 1)]
     
-    # Variablen:
-    # - password: string
-    # - ruleLetter: string
-    # - firstPos, secondPos: int
-    foundOccurences = 0
+    return (password[firstPos - 1] == ruleLetter) ^ (password[secondPos - 1] == ruleLetter)
     
-    # Erste Position prüfen (1-Indiziert vs 0-Indiziert bei Python)
-    try:
-        if password[firstPos - 1] == ruleLetter:
-            foundOccurences += 1
-    except:
-        # Out of bounds: Offensichtlich kein Treffer
-        pass
-    
-    
-    # Zweite Position prüfen (1-Indiziert vs 0-Indiziert bei Python)
-    try:
-        if password[secondPos - 1] == ruleLetter:
-            foundOccurences += 1
-    except:
-        # Out of bounds: Offensichtlich kein Treffer
-        pass
-    
-    # Exakt an einer Stelle (erste ODER zweite Position) muss der Buchstabe vorkommen
-    return foundOccurences == 1
-
 
 # Teil 2: Anzahl korrekter Passwörter nach neuen Regeln
 numCorrect = 0
